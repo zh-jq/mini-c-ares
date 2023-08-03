66bb857c0f466b1e9bb4f1b5215ac4ce4a556d66
zhangjingqiang <zhangjingqiang@bytedance.com>
2023-08-03 15:07:25 +0800

    fmt

--- c-ares-sys/build.rs
+++ c-ares-sys/build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    pkg_config::Config::new().atleast_version("1.11.0").probe("libcares").unwrap();
+    pkg_config::Config::new()
+        .atleast_version("1.11.0")
+        .probe("libcares")
+        .unwrap();
 }
