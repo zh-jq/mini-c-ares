45e03e8109ad8a534b89e9662e042fadb92f2425
zhangjingqiang <zhangjingqiang@bytedance.com>
2023-08-28 16:10:24 +0800

    add vendored feature

--- c-ares-sys/build.rs
+++ c-ares-sys/build.rs
@@ -1,6 +1,14 @@
+
+#[cfg(not(features = "vendored"))]
 fn main() {
     pkg_config::Config::new()
         .atleast_version("1.12.0")
         .probe("libcares")
         .unwrap();
 }
+
+#[cfg(features = "vendored")]
+fn main() {
+    c_ares_src::build();
+}
+
